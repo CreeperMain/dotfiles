659df14b601d3959df2d819eade6a9d1e81d4fe8
CreeperMain <creepermaingaming@gmail.com>
2023-09-06 19:06:47 +0200

    added script additions

--- likehome/Desktop/get-out-of-record.sh
+++ likehome/Desktop/get-out-of-record.sh
@@ -2,3 +2,4 @@
 cp /home/martin/.dotfiles/likehome/.xinitrc-normalno /home/martin/.dotfiles/likehome/.xinitrc
 rm -rf /etc/X11/xorg.conf
 mv /etc/X11/.xorg.conf.d /etc/X11/xorg.conf.d
+killall X
--- likehome/Desktop/record-script.sh
+++ likehome/Desktop/record-script.sh
@@ -2,3 +2,4 @@
 cp /home/martin/.dotfiles/likehome/.xinitrc-snimanje /home/martin/.dotfiles/likehome/.xinitrc
 cp /home/martin/Desktop/put-this-int.etc.X11-aku\ sakash\ da\ snimash/xorg.conf /etc/X11/
 mv /etc/X11/xorg.conf.d /etc/X11/.xorg.conf.d
+killall X
